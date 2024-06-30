da2372b69535b61d26f03162720dd9960e47cc7c
Abdul Hafiz Ramadan <abdulhafizramadan.dev@gmail.com>
2024-06-30 12:28:48 +0700

    Initial Commit

---
+++ CollectionViewInUITableViewStoryBoardUITests/CollectionViewInUITableViewStoryBoardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CollectionViewInUITableViewStoryBoardUITestsLaunchTests.swift
+//  CollectionViewInUITableViewStoryBoardUITests
+//
+//  Created by Abdul Hafiz Ramadan on 30/06/24.
+//
+
+import XCTest
+
+final class CollectionViewInUITableViewStoryBoardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
